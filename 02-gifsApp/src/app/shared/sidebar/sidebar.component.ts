46b625b72404608eb69560778ebce128226e2fff
la1314 <tsue00@gmail.com>
2022-05-10 21:42:53 +0200

    App Gifs

---
+++ 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent  {
+
+  constructor(private gifsService:GifsService) { }
+
+  get historial():string[] {
+    return this.gifsService.historial;
+  }
+  
+
+}
